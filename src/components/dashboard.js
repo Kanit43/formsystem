a52b0e6aaa8bcc548c4ee12efca9a20f3c9dd7e3
Kanit43 <kenging260@gmail.com>
2023-03-26 21:36:25 +0700

    delete attem and icon form

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -26,15 +26,15 @@ const Dashboard = () => {
   }
 
   const formList = [];
-  for (let i = 1; i <= 8; i++) {
-    formList.push(
-      <div key={"form" + i} className="col-sm-2">
-        <Link className="btn btn-outline-primary" to={"/form/" + i}>
-          Form{i}
-        </Link>
-      </div>
-    );
-  }
+  // for (let i = 1; i <= 8; i++) {
+  //   formList.push(
+  //     <div key={"form" + i} className="col-sm-2">
+  //       <Link className="btn btn-outline-primary" to={"/form/" + i}>
+  //         Form{i}
+  //       </Link>
+  //     </div>
+  //   );
+  // }
 
   return (
     <Container>
@@ -57,15 +57,7 @@ const Dashboard = () => {
         </tbody>
       </Table>
       {formList}
-      <div> <font color="red"> *หมายเหตุ </font></div>
-      <div><font color="red"> form1 หมายถึง คำร้องทั่วไป เรียนคณบดี </font></div>
-      <div></div><font color="red"> form2 หมายถึง คำร้องทั่วไป เรียนรองอธิการบดี </font>
-      <div></div><font color="red"> form3 หมายถึง คำร้องขอลาพักการศึกษา-รักษาสถานภาพนักศึกษา </font>
-      <div></div><font color="red"> form4 หมายถึง คำร้องขอผ่อนผันค่าใช้จ่ายทางการศึกษา 65 </font>
-      <div></div><font color="red"> form5 หมายถึง คำร้องขอเปลี่ยนค่าใช้จ่าย </font>
-      <div></div><font color="red"> form6 หมายถึง คำร้องขอลงทะเบียนเรียนล่าช้า </font>
-      <div></div> <font color="red"> form7 หมายถึง บันทึกแจ้งเปลี่ยนคะแนน I </font>
-      <div></div><font color="red"> form8 หมายถึง คำร้องขอเปิดรายวิชา </font>
+     
       <div><a href="https://forms.gle/YfgR1ebF5Vn9dGvV8">แบบประเมินความพึงพอใจ</a></div>
       </Container>
   );
